56ab943e0c0fa7675aea3ac631c3764acb0ec714
= <fernando@soluciones3f.com.ar>
2015-02-23 23:48:55 -0300

    Initial basic version

---
+++ change_credentials.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for, request, flash, redirect
+from passlib.apache import HtpasswdFile
+
+from string import ascii_letters, digits
+from random import choice
+from crypt import crypt
+
+app = Flask(__name__)
+app.secret_key = 'super dupper secret_mega_key'
+
+@app.route("/")
+def ask_credentials():
+    url = url_for('change_credential')
+    return render_template("credentials.html", change_url=url)
+
+
+@app.route("/", methods=["POST"])
+def change_credential():
+    username = request.form['username']
+    oldpassword = request.form['oldpassword']
+    newpassword = request.form['newpassword']
+
+    if _check_current_password(username, oldpassword):
+        _change_password(username, newpassword)
+        flash('Your password have been changed.')
+        return redirect(url_for('ask_credentials'))
+
+    return "Error changing yor password"
+
+def _check_current_password(username, password):
+    ht = HtpasswdFile("devel_users.passwd")
+    return ht.check_password(username, password)
+
+def _change_password(username, newpassword):
+    ht = HtpasswdFile("devel_users.passwd")
+    ht.set_password(username, newpassword)
+    ht.save()
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
