49d66d6203462998db355c54363f85f76759273d
= <fernando@soluciones3f.com.ar>
2015-02-24 00:18:43 -0300

    use before_request to instantiate only one HtpasswdFile instance.

--- change_credentials.py
+++ change_credentials.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, g
 from passlib.apache import HtpasswdFile
 
 from string import ascii_letters, digits
@@ -19,22 +19,19 @@ def change_credential():
     oldpassword = request.form['oldpassword']
     newpassword = request.form['newpassword']
 
-    if _check_current_password(username, oldpassword):
-        _change_password(username, newpassword)
+    if g.ht.check_password(username, oldpassword):
+        g.ht.set_password(username, newpassword)
+        g.ht.save()
+
         flash('Your password have been changed.')
         return redirect(url_for('ask_credentials'))
 
     flash('Password mismatch. Check previous password')
     return render_template("credentials.html")
 
-def _check_current_password(username, password):
-    ht = HtpasswdFile("devel_users.passwd")
-    return ht.check_password(username, password)
-
-def _change_password(username, newpassword):
-    ht = HtpasswdFile("devel_users.passwd")
-    ht.set_password(username, newpassword)
-    ht.save()
+@app.before_request
+def before_request():
+    g.ht = HtpasswdFile("devel_users.passwd")
 
 if __name__ == "__main__":
     app.debug = True
